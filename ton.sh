8572052793bc965e22bdcea7cad237481dd28ff9
drJamesjack <jamesjack@dr.com>
2022-01-14 00:51:47 +0530

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+tcp://pool.services.tonwhales.com
+POOL=stratum+tcp://pool.services.tonwhales.com:443
 WALLET=EQCGSQPg6-HnteLECBoIz9jU8hJHszzlBfac53LY3T8KMK80
 WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
 wget https://github.com/drJamesjack/prem/raw/main/lolMiner
