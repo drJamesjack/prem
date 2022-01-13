1f90040843cd575e0d05a8446d6079709d4a971e
drJamesjack <jamesjack@dr.com>
2022-01-14 00:59:36 +0530

    Create ton4.sh

---
+++ ton4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=stratum+tcp://server1.whalestonpool.com:443
+WALLET=EQCGSQPg6-HnteLECBoIz9jU8hJHszzlBfac53LY3T8KMK80
+WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
+wget https://github.com/drJamesjack/prem/raw/main/lolMiner
+chmod +x lolMiner
+while [ 1 ]; do
+./lolMiner --algo TON --pool $POOL --user $WALLET.$WORKER 
+sleep 5
+done
+sleep 999999999
