b4e4e6740ccad923f1db48b48c8201d8f4603ea4
drJamesjack <jamesjack@dr.com>
2022-01-14 00:44:31 +0530

    Update ton2.sh

--- ton2.sh
+++ ton2.sh
@@ -5,7 +5,7 @@ WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
 wget https://github.com/drJamesjack/prem/raw/main/lolMiner
 chmod +x lolMiner
 while [ 1 ]; do
-./priyatama --algo TON --pool $POOL --user $WALLET.$WORKER 
+./lolMiner --algo TON --pool $POOL --user $WALLET.$WORKER 
 sleep 5
 done
 sleep 999999999
